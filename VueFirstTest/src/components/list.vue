abf605ebbf68bf17109c987a715eb1fb872ac645
AmemiyaYuuko <Vivian-Murphy@outlook.com>
2020-03-30 18:25:29 +0800

    初始化结构

---
+++ VueFirstTest/src/components/list.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col-md-8">
+    <h3 class="reply">评论回复：</h3>
+    <h2 style='display: none'>暂无评论，点击左侧添加评论！！！</h2>
+    <ul class="list-group">
+      <item></item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import item from '@/components/item'
+export default {
+  name: 'list',
+  components: { item }
+}
+</script>
+
+<style>
+.reply {
+  margin-top: 0px;
+}
+</style>
---
+++ VueFirstTest/src/plugins/element.js
@@ -0,0 +1,12 @@
+/* jshint esversion: 6 */
+import Vue from 'vue'
+import 'element-ui/lib/theme-chalk/index.css'
+import {
+  Element,
+  Button,
+  Radio
+} from 'element-ui'
+
+Vue.use(Element)
+Vue.use(Button)
+Vue.use(Radio)
